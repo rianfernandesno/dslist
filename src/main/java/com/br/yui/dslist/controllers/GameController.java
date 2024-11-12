1d5f927626d878591e1c835907f53f764d57148c
rianfernandesno <zrpapz12@gmail.com>
2024-11-12 11:47:10 -0300

    gameDTO and findbyID

--- src/main/java/com/br/yui/dslist/controllers/GameController.java
+++ src/main/java/com/br/yui/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.br.yui.dslist.controllers;
 
+import com.br.yui.dslist.dto.GameDTO;
 import com.br.yui.dslist.dto.GameMinDTO;
 import com.br.yui.dslist.entities.Game;
 import com.br.yui.dslist.services.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,15 @@ public class GameController {
     public GameController(GameService gameService){
         this.gameService = gameService;
     }
+
     @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
         return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/br/yui/dslist/services/GameService.java
+++ src/main/java/com/br/yui/dslist/services/GameService.java
@@ -1,9 +1,11 @@
 package com.br.yui.dslist.services;
 
+import com.br.yui.dslist.dto.GameDTO;
 import com.br.yui.dslist.dto.GameMinDTO;
 import com.br.yui.dslist.entities.Game;
 import com.br.yui.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public class GameService {
     public GameService(GameRepository gameRepository){
         this.gameRepository  = gameRepository;
     }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> listGame = gameRepository.findAll();
         List<GameMinDTO> dto = listGame.stream()
@@ -22,4 +26,13 @@ public class GameService {
                 .toList();
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+       Game result = gameRepository.findById(id).get();
+       GameDTO dto = new GameDTO(result);
+       return dto;
+
+    }
+
 }
